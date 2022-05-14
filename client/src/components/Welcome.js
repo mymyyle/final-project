2737f817c516e7254340dd9e45c0ec7e5e5032e2
Sang Nguyen <sangnguyen5697@gmail.com>
2022-05-14 19:51:57 +0700

    Chore: update welcome page

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,162 +1,105 @@
-import { Box } from "@mui/system";
-import {
-  Button,
-  Card,
-  Container,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
-import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
-import React from "react";
+import { Box } from '@mui/system';
+import { Container, Stack, Typography, useTheme } from '@mui/material';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import React from 'react';
+import WelcomeImage from './WelcomeImage';
+import WelcomeStats from './WelcomeStats';
 
 const Welcome = () => {
+  const theme = useTheme();
   return (
     <Container
-      maxWidth="lg"
-      sx={{ m: "3rem auto", fontFamily: "Lato, sans-serif", lineHeight: "1.6" }}
+      maxWidth='lg'
+      sx={{ m: '3rem auto', fontFamily: 'Lato, sans-serif', lineHeight: '1.6' }}
     >
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "2.5rem",
-          flexDirection: { xs: "column", md: "row" },
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '2.5rem',
+          flexDirection: { xs: 'column', md: 'row' },
         }}
       >
-        <Box
-          component="img"
-          src="https://i.imgur.com/YB7qE8w.png"
-          alt=""
-          sx={{ width: { sm: "100%" } }}
-        />
-        <Stack spacing={2.5}>
-          <Typography variant="b1" sx={{ color: "#ff7675", fontWeight: 650 }}>
+        <WelcomeImage />
+        <Stack spacing={2.5} sx={{ flex: 1 }}>
+          <Typography
+            variant='b1'
+            sx={{ color: theme.palette.main, fontWeight: 650 }}
+          >
             _________ Welcome to our Community
           </Typography>
           <Typography
             // variant="h3"
-            sx={{ fontWeight: 800, fontSize: "45px", lineHeight: "60px" }}
+            sx={{ fontWeight: 800, fontSize: '45px', lineHeight: '60px' }}
           >
             Helping each other can make world better
           </Typography>
 
-          <Typography variant="b1 " sx={{ color: "#707876" }}>
+          <Typography variant='b1' sx={{ color: '#707876' }}>
             Lorem ipsum dolor sit amet, consectetur notted adipisicing elit sed
             do eiusmod tempor incididunt ut labore et simply free text dolore
             magna aliqua lonm andhn.
           </Typography>
           <Box
-            id="mission-story"
+            id='mission-story'
             sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "start",
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'start',
             }}
           >
-            <Box id="mission">
+            <Box id='mission'>
               <Typography
                 sx={{
-                  mb: "0.5rem",
-                  display: "flex",
-                  alignItems: "center",
+                  mb: '0.5rem',
+                  display: 'flex',
+                  alignItems: 'center',
                 }}
               >
                 <ArrowCircleRightIcon
-                  htmlColor="#ff7675"
-                  sx={{ fontSize: "30px" }}
+                  htmlColor={theme.palette.main}
+                  sx={{ fontSize: '30px' }}
                 />
                 <Typography
-                  component="span"
-                  sx={{ ml: "0.75rem", fontWeight: 600 }}
+                  component='span'
+                  sx={{ ml: '0.75rem', fontWeight: 600 }}
                 >
                   Our Mission
                 </Typography>
               </Typography>
 
-              <Typography variant="b1 " sx={{ color: "#707876" }}>
+              <Typography variant='b1 ' sx={{ color: '#707876' }}>
                 Lorem ipsum dolor sit amet not is consectetur notted.
               </Typography>
             </Box>
-            <Box id="story">
+            <Box id='story'>
               <Typography
                 sx={{
-                  mb: "0.5rem",
-                  display: "flex",
-                  alignItems: "center",
+                  mb: '0.5rem',
+                  display: 'flex',
+                  alignItems: 'center',
                 }}
               >
                 <ArrowCircleRightIcon
-                  htmlColor="#ff7675"
-                  sx={{ fontSize: "30px" }}
+                  htmlColor={theme.palette.main}
+                  sx={{ fontSize: '30px' }}
                 />
                 <Typography
-                  component="span"
-                  sx={{ ml: "0.75rem", fontWeight: 600 }}
+                  component='span'
+                  sx={{ ml: '0.75rem', fontWeight: 600 }}
                 >
                   Our Story
                 </Typography>
               </Typography>
 
-              <Typography variant="b1 " sx={{ color: "#707876" }}>
+              <Typography variant='b1 ' sx={{ color: '#707876' }}>
                 Lorem ipsum dolor sit amet not is consectetur notted.
               </Typography>
             </Box>
           </Box>
-          <Box id="charity">
-            <Typography sx={{ fontWeight: 600, mb: "0.5rem" }}>
-              Charity
-            </Typography>
-            <Box
-              sx={{
-                width: "90%",
-                height: "15px",
-                bgcolor: "#eff5f4",
-                borderRadius: "8px",
-              }}
-            >
-              <Box
-                sx={{
-                  width: "53%",
-                  height: "15px",
-                  bgcolor: "#ff7675",
-                  borderRadius: "8px",
-                }}
-              />
-              <Typography
-                sx={{ ml: "53%", color: "#707876", fontSize: "16px" }}
-              >
-                53%
-              </Typography>
-            </Box>
-          </Box>
-          <Box id="Donations">
-            <Typography sx={{ fontWeight: 600, mb: "0.5rem" }}>
-              Donations
-            </Typography>
-            <Box
-              sx={{
-                width: "90%",
-                height: "15px",
-                bgcolor: "#eff5f4",
-                borderRadius: "8px",
-              }}
-            >
-              <Box
-                sx={{
-                  width: "79%",
-                  height: "15px",
-                  bgcolor: "#ff7675",
-                  borderRadius: "8px",
-                }}
-              />
-              <Typography
-                sx={{ ml: "79%", color: "#707876", fontSize: "16px" }}
-              >
-                79%
-              </Typography>
-            </Box>
-          </Box>
+          <WelcomeStats text='Charity' percent='53%' />
+          <WelcomeStats text='Donations' percent='79%' />
+          <WelcomeStats text='Goal' percent='42%' />
         </Stack>
       </Box>
     </Container>
--- client/src/theme/index.js
+++ client/src/theme/index.js
@@ -1,104 +1,123 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline } from '@mui/material';
 import {
   alpha,
   createTheme,
   ThemeProvider as MUIThemeProvider,
-} from "@mui/material/styles";
-import { createContext, useMemo, useState } from "react";
-import customizeComponents from "./customizations";
+} from '@mui/material/styles';
+import { createContext, useMemo, useState } from 'react';
+import customizeComponents from './customizations';
+
+import ReeyRegular from 'assets/Reey-Regular.otf';
 
 const PRIMARY = {
-  lighter: "#C8FACD",
-  light: "#5BE584",
-  main: "#00AB55",
-  dark: "#007B55",
-  darker: "#005249",
-  contrastText: "#FFF",
+  lighter: '#C8FACD',
+  light: '#5BE584',
+  main: '#00AB55',
+  dark: '#007B55',
+  darker: '#005249',
+  contrastText: '#FFF',
 };
 const PRIMARY_1 = {
-  lighter: "#E3F2FD",
-  light: "#90CAF9",
-  main: "#2196F3",
-  dark: "#1E88E5",
-  darker: "#1565C0",
-  contrastText: "#FFF",
+  lighter: '#E3F2FD',
+  light: '#90CAF9',
+  main: '#2196F3',
+  dark: '#1E88E5',
+  darker: '#1565C0',
+  contrastText: '#FFF',
 };
 const SECONDARY = {
-  lighter: "#D6E4FF",
-  light: "#84A9FF",
-  main: "#3366FF",
-  dark: "#1939B7",
-  darker: "#091A7A",
-  contrastText: "#FFF",
+  lighter: '#D6E4FF',
+  light: '#84A9FF',
+  main: '#3366FF',
+  dark: '#1939B7',
+  darker: '#091A7A',
+  contrastText: '#FFF',
 };
 const SECONDARY_1 = {
-  lighter: "#ede7f6",
-  light: "#b39ddb",
-  main: "#673ab7",
-  dark: "#5e35b1",
-  darker: "#4527a0",
-  contrastText: "#FFF",
+  lighter: '#ede7f6',
+  light: '#b39ddb',
+  main: '#673ab7',
+  dark: '#5e35b1',
+  darker: '#4527a0',
+  contrastText: '#FFF',
 };
 const SUCCESS = {
-  lighter: "#E9FCD4",
-  light: "#AAF27F",
-  main: "#54D62C",
-  dark: "#229A16",
-  darker: "#08660D",
-  contrastText: "#FFF",
+  lighter: '#E9FCD4',
+  light: '#AAF27F',
+  main: '#54D62C',
+  dark: '#229A16',
+  darker: '#08660D',
+  contrastText: '#FFF',
 };
 const SUCCESS_1 = {
-  lighter: "#b9f6ca",
-  light: "#69f0ae",
-  main: "#69f0ae",
-  dark: "#00c853",
-  darker: "#08660D",
-  contrastText: "#FFF",
+  lighter: '#b9f6ca',
+  light: '#69f0ae',
+  main: '#69f0ae',
+  dark: '#00c853',
+  darker: '#08660D',
+  contrastText: '#FFF',
 };
 
 const GREY = {
-  0: "#FFFFFF",
-  100: "#F9FAFB",
-  200: "#F4F6F8",
-  300: "#DFE3E8",
-  400: "#C4CDD5",
-  500: "#919EAB",
-  600: "#637381",
-  700: "#454F5B",
-  800: "#212B36",
-  900: "#161C24",
-  500_8: alpha("#919EAB", 0.08),
-  500_12: alpha("#919EAB", 0.12),
-  500_16: alpha("#919EAB", 0.16),
-  500_24: alpha("#919EAB", 0.24),
-  500_32: alpha("#919EAB", 0.32),
-  500_48: alpha("#919EAB", 0.48),
-  500_56: alpha("#919EAB", 0.56),
-  500_80: alpha("#919EAB", 0.8),
+  0: '#FFFFFF',
+  100: '#F9FAFB',
+  200: '#F4F6F8',
+  300: '#DFE3E8',
+  400: '#C4CDD5',
+  500: '#919EAB',
+  600: '#637381',
+  700: '#454F5B',
+  800: '#212B36',
+  900: '#161C24',
+  500_8: alpha('#919EAB', 0.08),
+  500_12: alpha('#919EAB', 0.12),
+  500_16: alpha('#919EAB', 0.16),
+  500_24: alpha('#919EAB', 0.24),
+  500_32: alpha('#919EAB', 0.32),
+  500_48: alpha('#919EAB', 0.48),
+  500_56: alpha('#919EAB', 0.56),
+  500_80: alpha('#919EAB', 0.8),
 };
 
 export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 function ThemeProvider({ children }) {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState('light');
   const themeOptions = (mode) =>
-    mode === "light"
+    mode === 'light'
       ? {
+          typography: {
+            fontFamily: ['"Helvetica Neue"', 'Arial', 'sans-serif'].join(','),
+          },
+          components: {
+            MuiCssBaseline: {
+              styleOverrides: `
+              @font-face {
+                font-family: 'Reey';
+                font-style: normal;
+                font-display: swap;
+                font-weight: 400;
+                src: local(Reey-Regular), url(${ReeyRegular}) format('otf');
+                unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+              }
+            `,
+            },
+          },
           palette: {
             primary: PRIMARY,
             secondary: SECONDARY,
             success: SUCCESS,
-            header: "#fff",
-            comment: "rgb(240, 242, 245)",
-
+            header: '#fff',
+            comment: 'rgb(240, 242, 245)',
+            main: '#ff7675',
             text: {
               primary: GREY[800],
               secondary: GREY[600],
               disabled: GREY[500],
             },
             background: {
-              paper: "#fff",
-              default: "rgba(230, 230, 230,0.05)",
+              paper: '#fff',
+              default: 'rgba(230, 230, 230,0.05)',
               neutral: GREY[200],
             },
             action: {
@@ -116,15 +135,15 @@ function ThemeProvider({ children }) {
         }
       : {
           palette: {
-            mode: "dark",
+            mode: 'dark',
             primary: PRIMARY_1,
             secondary: SECONDARY_1,
             success: SUCCESS_1,
-            header: "rgb(31,29,25)",
-            comment: "#3d3d3d",
+            header: 'rgb(31,29,25)',
+            comment: '#3d3d3d',
 
             background: {
-              default: "rgba(43, 42, 51)",
+              default: 'rgba(43, 42, 51)',
             },
           },
         };
@@ -132,7 +151,7 @@ function ThemeProvider({ children }) {
   const colorMode = useMemo(
     () => ({
       toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
       },
     }),
     []
